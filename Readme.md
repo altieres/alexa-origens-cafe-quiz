a32b9ba123ea50309e6b89496d50f9da7fe36d73
Altieres Lopes <altiereslopes@webgoal.com.br>
2019-12-03 21:51:15 -0300

    initial commit Merge branch 'master' of bitbucket.org:floz-team/mobile-consumer

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Skill Alexa do Origens Café
+
+Ensine à Alexa uma grande quantidade de questões de multipla escolha sobre o universo do Café: suas origens, agronomia e a bebida na xícara.
---
+++ lambda/custom/questions.js
@@ -0,0 +1,116 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_PT_BR: [
+    {
+      'Em que ano o café chegou ao Brasil?': [
+        '1727',
+        '1500',
+        '1622',
+        '1821',
+      ]
+    }, {
+      'O grão de café tem qual cor quando está maduro?': [
+        'Vermelho ou Amarelo',
+        'Verde ou Amarelo',
+        'Amarelo ou Verde',
+        'Preto',
+      ]
+    }, {
+      'Você sabia que no Brasil o café ganhou uma data especial para ser celebrado? Qual é o Dia Nacional do Café?': [
+        '24 de maio',
+        '12 de outubro',
+        '25 de dezembro',
+        '17 de julho',
+      ]
+    }, {
+      'Em média, quantas xícaras de café um brasileiro consome por ano?': [
+        '1200',
+        '820',
+        '630',
+        '240',
+      ]
+    }, {
+      'Qual o nome do profissional especializado em cafés?': [
+        'Barista',
+        'Sommelier',
+        'Agricultor',
+        'Agrônomo',
+      ]
+    }, {
+      'Qual o estado brasileiro maior produtor de café?': [
+        'Minas Gerais',
+        'Paraná',
+        'Rio de Janeiro',
+        'Bahia',
+      ]
+    }, {
+      'Em qual país se originou o fruto café?': [
+        'Etiópia',
+        'Brasil',
+        'Colômbia',
+        'Arábia',
+      ]
+    }, {
+      'Sabemos que o Brasil é o maior produtor de café do mundo. Qual país está em segundo lugar?': [
+        'Vietnã',
+        'Colômbia',
+        'Etiópia',
+        'México',
+      ]
+    }, {
+      'Em relação às espécies da planta do café, qual afirmativa está correta?': [
+        'Arábica e Robusta são as espécies com valor comercial',
+        'Robusta é uma espécie que não produz frutos',
+        'Arábica é uma espécie que só produz no Brasil',
+        'Existem apenas as espécies Arábica e Robusta',
+      ]
+    }, {
+      'Qual o nome que se dá ao fruto do café logo após deixar de ser uma flor mas ainda antes de ser considerado um grão de café verde?': [
+        'Chumbinho',
+        'Flor maturada',
+        'Cereja',
+        'Passa',
+      ]
+    }, {
+      'Bourbon, Catuaí e Mundo Novo diezem respeito a qual dessas classificações?': [
+        'Variedades',
+        'Espécies',
+        'Famílias',
+        'Grau de maturação',
+      ]
+    }, {
+      'A classificação da pior bebida do café até a melhor, pode ser resumidamente definida como:': [
+        'Rio, dura e mole',
+        'Dura, rio e mole',
+        'Ruim, boa e ótima',
+        'Azedo, ácido e doce',
+      ]
+    }, {
+      'Qual  o profissional de provador de café possa classificar a acidez, doçura, amargor, corpo e outras nuances do café?': [
+        'Sorve o café da xícara com força, fazendo barulho',
+        'Descarta o café, ao invés de engolir',
+        'Faz gargarejo com o café na boca',
+        'Deixa o café na boca por um minuto ou mais',
+      ]
+    }, {
+      'Uma torra muito escura, muitas vezes chamada de extra forte, normalmente é usada com qual objetivo?': [
+        'Esconder defeitos do café',
+        'Fazer o café render mais',
+        'Aumentar a qualidade do grão',
+        'Aumentar a validade do produto',
+      ]
+    }, {
+      'Qual o papel da crema, aquela espuminha, numa xícara da café espresso?': [
+        'Preservar o aroma e a temperatura',
+        'Permitir fazer desenhos artísticos',
+        'Não faz diferença',
+        'Caracteriza um café ruim',
+      ]
+    },
+  ],
+};
